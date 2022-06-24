22c3b5bec58aff1c38d8e6be1c383143dede45ae
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-06-24 19:37:13 +0100

    finalizacion clase viernes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './layouts/Layout';
+import PrivateLayout from './layouts/PrivateLayout';
 import { IndexPage } from './pages';
+import IndexAdmin from './pages/admin';
 import { Page2 } from './pages/page2';
 import { Page3 } from './pages/page3';
 
@@ -12,6 +14,9 @@ const App = () => {
           <Route index element={<IndexPage />} />
           <Route path='/pagina2' element={<Page2 />} />
           <Route path='/pagina3' element={<Page3 />} />
+        </Route>
+        <Route path='/admin' element={<PrivateLayout />}>
+          <Route path='/admin' element={<IndexAdmin />} />
         </Route>
       </Routes>
     </BrowserRouter>
