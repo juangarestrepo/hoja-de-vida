79f8586b8ef8ae4738347a499ee1f4874fcda651
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-07-25 12:38:00 +0100

    agregar navbar de color blanco

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => (
   <main className='h-screen flex flex-col'>
-    <nav className='bg-gray-700 p-4'>Navbar</nav>
+    <nav className='bg-gray-700 p-4 text-white'>Navbar</nav>
     <div className='h-full overflow-y-auto'>
       <Outlet />
     </div>
